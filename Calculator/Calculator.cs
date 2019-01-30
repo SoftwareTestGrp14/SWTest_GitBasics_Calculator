2a27dd276db0ec2be71cd8ef927c1ee3c25eb499
Thomas <thomas.moeller.1996@gmail.com>
2019-01-30 11:37:33 +0100

    Færdiggørelse af Add og Subtract

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -45,7 +46,13 @@ namespace Calculator
 
         public double Add(double addend)
         {
-            return 
+            return Accumulator + addend;
         }
+
+        public double Subtract(double subtractor)
+        {
+            return Accumulator - subtractor;
+        }
+
     }
 }
