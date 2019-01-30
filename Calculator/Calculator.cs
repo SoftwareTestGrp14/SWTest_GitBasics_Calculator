991d34630935431c0a6ee31f7a885e6ef4a51e10
Jonas <au544262@post.au.dk>
2019-01-30 10:55:56 +0100

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculator
+{
+    public class Calculator
+    {
+
+        public Calculator()
+        {
+
+        }
+
+        public double Add(double a, double b)
+        {
+
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp); ;
+        }
+
+    }
+}
